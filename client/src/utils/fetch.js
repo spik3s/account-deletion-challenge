cfad34bdcc93c8bf328b24fe501d587c95357cfb
Jacek 'Spikes' Wozniak <github@spik3s.com>
2019-10-13 03:33:15 +0700

    Extracted fetch operations into reusable functions
    Created simple get() and post() functions that have all the elements that are repeated each time we're making request

--- client/src/utils/fetch.js
+++ client/src/utils/fetch.js
@@ -1,6 +1,29 @@
 export const handleFetchErrors = response => {
-    if (!response.ok) throw Error(response.status);
-    return response;
+	if (!response.ok) throw Error(response.status);
+	return response;
 };
 
 export const fetchAbortController = new AbortController();
+
+export const get = target =>
+	window
+		.fetch(target, {
+			method: "GET",
+			mode: "cors",
+			signal: fetchAbortController.signal
+		})
+		.then(handleFetchErrors)
+		.then(response => response.json());
+
+export const post = (target, payload) =>
+	window
+		.fetch(target, {
+			method: "POST",
+			mode: "cors",
+			signal: fetchAbortController.signal,
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(payload)
+		})
+		.then(handleFetchErrors);
