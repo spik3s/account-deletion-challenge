4d4bfca11b119cea1e34a9b5fb6e6c50effa7bbd
Jacek 'Spikes' Wozniak <github@spik3s.com>
2019-10-14 18:57:45 +0700

    Removed unused code from LoadState.js
    File has a lot of code that we're not going to use in our app. States such as 'outdated' or 'refreshing' are unnecessary, as the data that we're fetching is unlikely candidate for persistance.

--- client/src/LoadState.js
+++ client/src/LoadState.js
@@ -3,29 +3,9 @@ import * as LOAD_STATE from "./constants/loadStatus"
 export const pending = { status: LOAD_STATE.PENDING }
 export const fetching = { status: LOAD_STATE.FETCHING }
 export const completed = { status: LOAD_STATE.COMPLETED }
-const outdated = { status: LOAD_STATE.OUTDATED }
-const refreshing = { status: LOAD_STATE.REFRESHING }
 export const error = { status: LOAD_STATE.ERROR }
+
 export const initWithError = error => ({ status: LOAD_STATE.ERROR, error })
-
 export const isError = state => state.status === LOAD_STATE.ERROR
-export const shouldLoad = state =>
-  state.status === LOAD_STATE.PENDING || state.status === LOAD_STATE.OUTDATED
 export const isLoading = state =>
-  // state.status === LOAD_STATE.PENDING ||
-  state.status === LOAD_STATE.FETCHING ||
-  state.status === LOAD_STATE.REFRESHING
-export const isLoadingFirstTime = state => state.status === LOAD_STATE.FETCHING
-export const isRefreshing = state => state.status === LOAD_STATE.REFRESHING
-export const isLoaded = state =>
-  state.status === LOAD_STATE.COMPLETED || state.status === LOAD_STATE.REFRESHING
-
-export const handleLoaded = state => completed
-export const handleOutdated = state => outdated
-export const handleLoadRequested = state => {
-  return state.status === LOAD_STATE.OUTDATED ? refreshing : fetching
-}
-export const handleLoadFailedWithError = error => {
-  const nextState = initWithError(error)
-  return state => nextState
-}
+state.status === LOAD_STATE.FETCHING
