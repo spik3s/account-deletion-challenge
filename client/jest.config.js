c965c929ac39aaabe155eaa8a7ed48ff11829add
Jacek 'Spikes' Wozniak <github@spik3s.com>
2019-10-15 11:31:37 +0700

    Added Enzyme to enable better testing

--- client/jest.config.js
+++ client/jest.config.js
@@ -123,7 +123,8 @@ module.exports = {
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
   setupFiles: [
-    "./jestModules.js"
+    "./jestModules.js",
+    "./enzymeSetup.js"
   ],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
