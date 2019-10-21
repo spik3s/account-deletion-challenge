a35a79772c094bdd9d495b95630d2fd2e9172882
Jacek 'Spikes' Wozniak <github@spik3s.com>
2019-10-21 10:03:34 +0700

    Updated FeedbackView tests to work after changes to FeedbackView
    Fix all issues introduced in commit 6989e2. Add additional lines in tests to test after changes in commit 19080b.

--- client/src/components/FeedbackView/FeedbackView.test.js
+++ client/src/components/FeedbackView/FeedbackView.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Component from ".";
-import { shallow, render, mount } from "enzyme";
+import Component, { FeedbackView } from ".";
+import { shallow, mount } from "enzyme";
 import { feedbackAnswers } from "../../data/feedbackAnswers";
+import { AppContext } from "../../AppContext";
 
 const feedbackDataEmpty = {
 	answers: [],
@@ -23,81 +24,101 @@ describe("FeedbackView", () => {
 	it("renders without crashing", () => {
 		const div = document.createElement("div");
 		ReactDOM.render(
-			<Component
-				feedbackData={feedbackDataEmpty}
-				onChangeFeedback={() => {}}
-			/>,
+			<AppContext.Provider
+				value={{
+					appState: { feedbackData: feedbackDataEmpty },
+					setAppState: () => {}
+				}}
+			>
+				<Component />
+			</AppContext.Provider>,
 			div
 		);
 		ReactDOM.unmountComponentAtNode(div);
 	});
 	it("calls onChangeFeedback() when checkbox is selected", () => {
-		const onChangeFeedback = jest.fn();
-		const wrapper = mount(
-			<Component
-				feedbackData={feedbackDataEmpty}
-				onChangeFeedback={onChangeFeedback}
+		const wrapper = shallow(
+			<FeedbackView
+				appState={{ feedbackData: feedbackDataEmpty }}
+				setAppState={() => {}}
 			/>
 		);
+		const instance = wrapper.instance();
+		jest.spyOn(instance, "onChangeFeedback");
+		jest.spyOn(instance, "updateFeedbackCheckedAnswers");
+		instance.forceUpdate();
 
 		wrapper
 			.find(`input[type="checkbox"][name="${feedbackAnswers[0].stack}"]`)
-			.simulate("change", { target: { checked: true } });
+			.simulate("change", { target: { checked: true, type: "checkbox" } });
 
-		expect(onChangeFeedback).toHaveBeenCalledTimes(1);
+		expect(instance.onChangeFeedback).toHaveBeenCalledTimes(1);
+		expect(instance.updateFeedbackCheckedAnswers).toHaveBeenCalledTimes(1);
 		wrapper.unmount();
 	});
 
-	it("calls onChangeFeedback() when typing in 'others' input field ", () => {
-		const onChangeFeedback = jest.fn();
-		const wrapper = mount(
-			<Component
-				feedbackData={feedbackDataEmpty}
-				onChangeFeedback={onChangeFeedback}
+	it("calls onChangeFeedback() & updateFeedbackOtherAnswerValue() when typing in 'others' input field ", () => {
+		const wrapper = shallow(
+			<FeedbackView
+				appState={{ feedbackData: feedbackDataEmpty }}
+				setAppState={() => {}}
 			/>
 		);
+		const event = {
+			target: { value: "some value", type: "text" }
+		};
+		const instance = wrapper.instance();
+		jest.spyOn(instance, "onChangeFeedback");
+		jest.spyOn(instance, "updateFeedbackOtherAnswerValue");
+		instance.forceUpdate();
 
 		expect(wrapper.find('input[name="others"]').exists()).toBeTruthy();
-		wrapper.find('input[name="others"]').simulate("change");
-		expect(onChangeFeedback).toHaveBeenCalledTimes(1);
+		wrapper.find('input[name="others"]').simulate("change", event);
+		expect(instance.onChangeFeedback).toHaveBeenCalledTimes(1);
+		expect(instance.updateFeedbackOtherAnswerValue).toHaveBeenCalledTimes(1);
 		wrapper.unmount();
 	});
 
-	it("calls onChangeFeedback() when comment is typed", () => {
-		const onChangeFeedback = jest.fn();
+	it("calls onChangeFeedback() & updateFeedbackComment() when comment is typed", () => {
 		const wrapper = mount(
-			<Component
-				feedbackData={feedbackDataEmpty}
-				onChangeFeedback={onChangeFeedback}
+			<FeedbackView
+				appState={{ feedbackData: feedbackDataEmpty }}
+				setAppState={() => {}}
 				showCommentForm={true}
 			/>
 		);
+		const event = {
+			target: { value: "some value", name: "comment" }
+		};
+		const instance = wrapper.instance();
+		jest.spyOn(instance, "onChangeFeedback");
+		jest.spyOn(instance, "updateFeedbackComment");
+		instance.forceUpdate();
 
-		wrapper.find("textarea").simulate("change");
-		expect(onChangeFeedback).toHaveBeenCalledTimes(1);
+		wrapper.find("textarea").simulate("change", event);
+		expect(instance.onChangeFeedback).toHaveBeenCalledTimes(1);
+		expect(instance.updateFeedbackComment).toHaveBeenCalledTimes(1);
 		wrapper.unmount();
 	});
 
 	it("doesn't display when comment field if showCommentForm is not true", () => {
-		const onChangeFeedback = jest.fn();
-		const wrapper = mount(
-			<Component
-				feedbackData={feedbackDataEmpty}
-				onChangeFeedback={onChangeFeedback}
+		const wrapper = shallow(
+			<FeedbackView
+				appState={{ feedbackData: feedbackDataEmpty }}
+				setAppState={() => {}}
 			/>
 		);
-		expect(wrapper.find('textarea').exists()).toBe(false);
+		expect(wrapper.find("textarea").exists()).toBe(false);
 		wrapper.unmount();
 	});
-
 
 	it("prevents from proceeding to next view without selecting at least one answer", () => {
 		const onClickNext = jest.fn();
 		const wrapper = mount(
-			<Component
-				feedbackData={feedbackDataEmpty}
+			<FeedbackView
 				onClickNext={onClickNext}
-				onChangeFeedback={() => {}}
+				appState={{ feedbackData: feedbackDataEmpty }}
+				setAppState={() => {}}
 			/>
 		);
 
@@ -105,16 +126,17 @@ describe("FeedbackView", () => {
 		expect(onClickNext).toHaveBeenCalledTimes(0);
 		wrapper.unmount();
 	});
-	
+
 	it("calls onClickNext() when clicked next if answer was selected", () => {
 		const onClickNext = jest.fn();
 		const wrapper = mount(
-			<Component
-				feedbackData={feedbackDataWithAnswer}
+			<FeedbackView
 				onClickNext={onClickNext}
-				onChangeFeedback={() => {}}
+				appState={{ feedbackData: feedbackDataWithAnswer }}
+				setAppState={() => {}}
 			/>
 		);
+
 		wrapper.find('button[children="Next"]').simulate("click");
 		expect(onClickNext).toHaveBeenCalledTimes(1);
 		wrapper.unmount();
